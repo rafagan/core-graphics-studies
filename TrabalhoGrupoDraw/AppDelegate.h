4ea14822f64960bca6b8fb9a9891a2fd48e2677c
Carlos Guimaraes <carlosguimaraes@MACarlos.local>
2013-11-14 16:11:44 -0200

    Initial Commit

---
+++ TrabalhoGrupoDraw/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrabalhoGrupoDraw
+//
+//  Created by Carlos Guimaraes on 14/11/13.
+//  Copyright (c) 2013 Carlos Guimaraes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrabalhoGrupoDraw/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TrabalhoGrupoDraw
+//
+//  Created by Carlos Guimaraes on 14/11/13.
+//  Copyright (c) 2013 Carlos Guimaraes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
